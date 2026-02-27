4fa6959ff8f1e0de546063fdc31c02961713da37
faiz <fk9653047@gmail.com>
2026-02-27 14:28:42 +0530

    fix27

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import loanTypesReducer from "./slices/loanTypesSlice";
 
+import {
+  persistStore,
+  persistReducer,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// 👇 Combine reducers
+const rootReducer = combineReducers({
+  auth: authReducer,
+  loanTypes: loanTypesReducer,
+});
+
+// 👇 Sirf auth persist karna hai
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"], // 👈 only auth will persist
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    loanTypes: loanTypesReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
